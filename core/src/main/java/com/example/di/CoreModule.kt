ab4d239f2c2affb9300fa1bbdf8ee1b1a5a3b635
Anggoro Beno <anggoro131098@gmail.com>
2022-06-18 02:49:21 +0700

    add chucker

--- core/src/main/java/com/example/di/CoreModule.kt
+++ core/src/main/java/com/example/di/CoreModule.kt
@@ -1,40 +1,35 @@
 package com.example.di
 
 import android.content.Context
+import com.chuckerteam.chucker.api.ChuckerInterceptor
 import com.example.core.BuildConfig
-import com.example.data.PokemonRepository
-import com.example.data.local.room.PokemonDB.Companion.getInstance
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import com.example.data.remote.network.ApiService
-import dagger.hilt.android.qualifiers.ApplicationContext
 import com.example.data.local.room.PokemonDB
+import com.example.data.local.room.PokemonDB.Companion.getInstance
 import com.example.data.local.room.PokemonDao
-import com.example.domain.repository.IPokemonRepository
+import com.example.data.remote.network.ApiService
 import com.example.utils.AppExecutors
 import dagger.Module
 import dagger.Provides
-import okhttp3.logging.HttpLoggingInterceptor.Level.BODY
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
 import retrofit2.Retrofit.Builder
+import retrofit2.converter.gson.GsonConverterFactory
 
 @Module
 @InstallIn(SingletonComponent::class)
 object CoreModule {
   private const val BASE_URL = "https://pokeapi.co/"
-   @Provides fun provideBaseUrl(): String {
+  @Provides fun provideBaseUrl(): String {
     return BASE_URL
   }
 
-   @Provides fun provideOkHttpClient(): OkHttpClient {
+  @Provides fun provideOkHttpClient(@ApplicationContext context: Context): OkHttpClient {
     return if (BuildConfig.DEBUG) {
-      val httpLoggingInterceptor = HttpLoggingInterceptor()
-      httpLoggingInterceptor.setLevel(BODY)
       OkHttpClient.Builder()
-        .addInterceptor(httpLoggingInterceptor)
+        .addInterceptor(ChuckerInterceptor(context))
         .build()
     } else {
       OkHttpClient.Builder()
@@ -42,7 +37,7 @@ object CoreModule {
     }
   }
 
-   @Provides fun provideRetrofit(client: OkHttpClient): Retrofit {
+  @Provides fun provideRetrofit(client: OkHttpClient): Retrofit {
     return Builder()
       .baseUrl(BASE_URL)
       .addConverterFactory(GsonConverterFactory.create())
@@ -50,21 +45,20 @@ object CoreModule {
       .build()
   }
 
-   @Provides fun provideApiService(retrofit: Retrofit): ApiService {
+  @Provides fun provideApiService(retrofit: Retrofit): ApiService {
     return retrofit.create(ApiService::class.java)
   }
 
-   @Provides fun provideDatabase(@ApplicationContext context: Context): PokemonDB {
+  @Provides fun provideDatabase(@ApplicationContext context: Context): PokemonDB {
     return getInstance(context)
   }
 
-   @Provides fun provideDao(db: PokemonDB): PokemonDao {
+  @Provides fun provideDao(db: PokemonDB): PokemonDao {
     return db.pokemonDao()
   }
 
   @Provides fun provideAppExecutors(): AppExecutors {
     return AppExecutors()
   }
-
 
 }
